f8e174bf0dc585524366e496e0f1e029fa905826
(Max) Loo Pin Mok <30368159+maxlooo@users.noreply.github.com>
2020-06-28 01:28:53 +0800

    Add files via upload

--- luhn1.hs
+++ luhn1.hs
@@ -3,7 +3,7 @@ checkNumber :: Int -> Bool
 checkNumber a = if a*2 > 9 then True
   else False
 luhnDouble a = if checkNumber a then a*2 - 9
-	else a*2
+  else a*2
 luhnSumMod a b c d = (luhnDouble a + b + luhnDouble c + d) `mod` 10
 luhn a b c d | luhnSumMod a b c d == 0 = True
              | otherwise = False
